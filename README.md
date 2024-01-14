748c37d03dea88a769fb96549dee3ff6f37aa429
saurabh3460 <saurabh3460@gmail.com>
2024-01-14 20:31:35 +0530

    wip: fix queryDatabaseSize

--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 docker run --rm -it --name postgres-container -e POSTGRES_USER=username -e POSTGRES_PASSWORD=quote -e POSTGRES_DB=yourdb -p 5432:5432 -d postgres
 ```
 
+```sh
+docker exec -it pgreloserverx_source-db_1 psql -U bookworm -d products
+```
--- main.go
+++ main.go
@@ -1,36 +1,84 @@
 package main
 
 import (
-    "database/sql"
-    "fmt"
-    "log"
-    _ "github.com/lib/pq"
+	"database/sql"
+	"flag"
+	"log"
+
+	_ "github.com/lib/pq"
 )
 
-func main() {
-    // Define the PostgreSQL connection string
-    connStr := "host=0.0.0.0 user=username password=quote dbname=yourdb sslmode=disable"
-    
-    // Open a connection to the database
-    db, err := sql.Open("postgres", connStr)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer db.Close()
+var (
+	sourceDB      string
+	destinationDB string
+	dbDriver      = "postgres"
+)
 
-    // Ping the database to ensure it's accessible
-    err = db.Ping()
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    // Query for the PostgreSQL server version
-    var version string
-    err = db.QueryRow("SELECT version()").Scan(&version)
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    fmt.Println(version)
+func init() {
+	flag.StringVar(&sourceDB, "source", "", "source database connection string")
+	flag.StringVar(&destinationDB, "dest", "", "destination database connection string")
 }
 
+func main() {
+
+	flag.Parse()
+
+	// Validate that sourceDB and destinationDB are not empty
+	if sourceDB == "" || destinationDB == "" {
+		log.Fatal("Both source and destination database connection strings must be provided.")
+	}
+
+	// Connect to the source database
+	sourceDBConn, err := sql.Open(dbDriver, sourceDB)
+	if err != nil {
+		log.Fatal("Error connecting to the source database:", err)
+	}
+	defer sourceDBConn.Close()
+
+	// Connect to the destination database
+	destinationDBConn, err := sql.Open(dbDriver, destinationDB)
+	if err != nil {
+		log.Fatal("Error connecting to the destination database:", err)
+	}
+	defer destinationDBConn.Close()
+
+	// Query and log the version for the source database
+	sourceVersion, err := queryDataaseVersion(sourceDBConn)
+	if err != nil {
+		log.Fatal("Error querying source database version:", err)
+	}
+	log.Printf("Source Database Version: %s", sourceVersion)
+
+	// Query and log the Size of the source database
+	sourceSize, err := queryDatabaseSize(sourceDBConn)
+	if err != nil {
+		log.Fatal("Error querying source database size:", err)
+	}
+	log.Printf("Source Database Size in Bytes: %d", sourceSize)
+
+	// Query and log the version for the destination database
+	destinationVersion, err := queryDataaseVersion(destinationDBConn)
+	if err != nil {
+		log.Fatal("Error querying destination database version:", err)
+	}
+	log.Printf("Destination Database Version: %s", destinationVersion)
+
+}
+
+func queryDataaseVersion(db *sql.DB) (string, error) {
+	var version string
+	err := db.QueryRow("SELECT version()").Scan(&version)
+	if err != nil {
+		return "", err
+	}
+	return version, nil
+}
+
+func queryDatabaseSize(db *sql.DB) (int64, error) {
+	var size int64
+	err := db.QueryRow("SELECT pg_database_size(current_database());").Scan(&size)
+	if err != nil {
+		return 0, err
+	}
+	return size, nil
+}
